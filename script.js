882428c50f3f031c5f2a2e0b207d7cf044894eb9
2413025032 <mutiarasalsabilaaya@gmail.com>
2025-03-10 22:13:30 +0700

    Update script.js

--- script.js
+++ script.js
@@ -42,9 +42,6 @@ navLinks.forEach((link) => {
 var canvas = document.getElementById('myCanvas');
 var ctx = canvas.getContext('2d');
 
-var centerX = canvas.width / 2;
-var centerY = canvas.height / 2;
-
 ctx.beginPath();
 ctx.moveTo(0, 0);
 ctx.lineTo(200,100);
