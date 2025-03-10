2f99015730591e8f5d8f0ac7f7780e231f0fb87a
2413025032 <mutiarasalsabilaaya@gmail.com>
2025-03-10 22:09:41 +0700

    Update script.js

--- script.js
+++ script.js
@@ -46,7 +46,7 @@ var centerX = canvas.width / 2;
 var centerY = canvas.height / 2;
 
 ctx.beginPath();
-ctx.moveTo(0, 0);
+ctx.moveTo(centerX, centerY);
 ctx.strokeStyle = 'black';
 ctx.lineWidth = 2;
 ctx.stroke();
